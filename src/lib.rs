25dcaff57bfa9ea29333bfeb71b9a2b69a9da27e
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2023-10-17 00:23:19 +0900

    GetFontData

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,21 @@ pub fn has_font_data(hdc: HDC, dwtable: u32, dwoffset: u32) -> bool {
     }
 }
 
+pub fn is_ttc(hdc: HDC) -> bool {
+    has_font_data(hdc, u32::from_be_bytes(*b"ttcf"), 0)
+}
+
+pub fn get_font_data_from_table_directory(hdc: HDC) -> Option<Vec<u8>> {
+    get_font_data(hdc, 0, 0)
+}
+
+pub fn get_font_file_data(hdc: HDC) -> Option<Vec<u8>> {
+    match get_font_data(hdc, u32::from_be_bytes(*b"ttcf"), 0) {
+        Some(data) => Some(data),
+        None => get_font_data_from_table_directory(hdc),
+    }
+}
+
 // ------------------------------
 // HDC
 // ------------------------------
