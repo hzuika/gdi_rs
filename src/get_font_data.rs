f2b9957e29c5f68f84792f14e51845e537e31de5
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2023-10-17 01:16:50 +0900

    GetFontData

--- src/get_font_data.rs
+++ src/get_font_data.rs
@@ -1,4 +1,9 @@
-use windows::Win32::Graphics::Gdi::{GetFontData, GDI_ERROR, HDC};
+use windows::Win32::Graphics::Gdi::{
+    CreateFontIndirectExW, CreateFontIndirectW, DeleteObject, GetFontData, SelectObject,
+    ENUMLOGFONTEXDVW, GDI_ERROR, HDC, LOGFONTW,
+};
+
+use crate::ManagedDC;
 
 pub fn get_font_data(hdc: HDC, dwtable: u32, dwoffset: u32) -> Option<Vec<u8>> {
     let size = unsafe { GetFontData(hdc, dwtable, dwoffset, None, 0) };
@@ -34,5 +39,37 @@ pub fn get_font_file_data(hdc: HDC) -> Option<Vec<u8>> {
     match get_font_data(hdc, u32::from_be_bytes(*b"ttcf"), 0) {
         Some(data) => Some(data),
         None => get_font_data_from_table_directory(hdc),
+    }
+}
+
+pub fn get_font_data_with_logfont(
+    logfont: &LOGFONTW,
+    dwtable: u32,
+    dwoffset: u32,
+) -> Option<Vec<u8>> {
+    unsafe {
+        let dc = ManagedDC::new(None);
+        let hfont = CreateFontIndirectW(logfont);
+        let old_hfont = SelectObject(dc.get_hdc(), hfont);
+        let result = get_font_data(dc.get_hdc(), dwtable, dwoffset);
+        let _ = SelectObject(dc.get_hdc(), old_hfont);
+        DeleteObject(hfont);
+        result
+    }
+}
+
+pub fn get_font_data_with_enum_logfont_ex_dv(
+    enum_logfont_ex_dv: &ENUMLOGFONTEXDVW,
+    dwtable: u32,
+    dwoffset: u32,
+) -> Option<Vec<u8>> {
+    unsafe {
+        let dc = ManagedDC::new(None);
+        let hfont = CreateFontIndirectExW(enum_logfont_ex_dv);
+        let old_hfont = SelectObject(dc.get_hdc(), hfont);
+        let result = get_font_data(dc.get_hdc(), dwtable, dwoffset);
+        let _ = SelectObject(dc.get_hdc(), old_hfont);
+        DeleteObject(hfont);
+        result
     }
 }
